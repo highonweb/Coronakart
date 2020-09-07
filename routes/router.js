61d348ec1feb888d91b27e0b445b7e3f60be9e13
Mohana Sundaram S <mohan@localhost.localdomain>
2020-09-06 20:06:45 -0700

    fixes

--- routes/router.js
+++ routes/router.js
@@ -2,22 +2,30 @@ const express = require('express');
 const router = express.Router();
 const mongoose = require('mongoose');
 const Item = require('../model/Item.js');
-router.post('/', async (req, res, next) => {});
-router.post('/seller', async (req, res, next) => {});
+router.post('/signin', async (req, res, next) => {});
+router.post('/buyer', async (req, res, next) => {});
+
+router.post('/buyer', async (req, res, next) => {});
 
 router.get('/', async (req, res, next) => {
   return res.render('index');
 });
 router.get('/seller', async (req, res, next) => {
   try {
-    console.log(req.session);
-    return res.send(req.session.mongoDB);
+    return res.render('seller');
+  } catch (error) {
+    console.log(error);
+  }
+});
+router.get('/buyer', async (req, res, next) => {
+  try {
+    return res.render('buyer');
   } catch (error) {
     console.log(error);
   }
 });
 
-router.get('/login', async (req, res, next) => {
+router.get('/signin', async (req, res, next) => {
   try {
     return res.render('login');
   } catch (error) {}
